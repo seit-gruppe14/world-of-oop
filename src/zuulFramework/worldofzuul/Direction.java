6be04f302cb85a4381bc415dcb75de87f32bc187
Rasmus Hansen <rhdh@digizuite.com>
2016-10-03 10:22:27 +0200

    Add comments to consts for directions

--- src/zuulFramework/worldofzuul/Direction.java
+++ src/zuulFramework/worldofzuul/Direction.java
@@ -1,11 +1,27 @@
 package zuulFramework.worldofzuul;
 
 /**
- * Created by Rasmus Hansen .
+ * Defines some constants for different directions it's possible to go between rooms
+ * Directions are cardinal.
  */
 public final class Direction {
+    /**
+     * Defines a direction to the east
+     */
     public final static String EAST = "east";
+
+    /**
+     * Defines a direction to the west
+     */
     public final static String WEST = "west";
+
+    /**
+     * Defines a direction to the north
+     */
     public final static String NORTH = "north";
+
+    /**
+     * Defines a direction to the south
+     */
     public final static String SOUTH = "south";
 }
