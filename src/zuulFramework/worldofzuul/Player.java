854af14cddfaa915339e4bb0612c54fec00c880b
SanderumChristian <chund.Christianjohansen@gmail.com>
2016-10-05 14:45:27 +0200

    Modified Player.java
    Added some comments, and modified some methods

--- src/zuulFramework/worldofzuul/Player.java
+++ src/zuulFramework/worldofzuul/Player.java
@@ -12,44 +12,81 @@ import java.util.List;
  * @author Christian
  */
 public class Player {
+
+    //TODO DISCUSS COMMENTS
+    /**
+     * Constructs the player with 100 HP.
+     */
     private int life = 100;
+    /**
+     * Constructs the attribute money.
+     */
     private int money;
+    /**
+     * Constructs the player item list.
+     */
     private List<Item> items = new ArrayList<Item>();
+    /**
+     * Constructs the current room of the player.
+     */
     private Room currentRoom;
+    /**
+     * Constructs the player's maximum carry weight.
+     */
+    //TODO ADD A REAL CARRY WEIGHT LIMIT
+    private static final double CARRY_WEIGHT = 100.0;
 
+    /**
+     *
+     * @return CARRY_WEIGHT which is a double representation of the player max
+     * carry weight.
+     */
     public double getCarryWeight() {
-        return 0;
+        return CARRY_WEIGHT;
     }
 
-    public void pickUp(String itemName) {
-        Item item = currentRoom.removeItem(itemName);
+    /**
+     * lets the player pick up items from the room they are in currently. If the
+     * item doesn't exist or isn't in the room, the method prints a message
+     * telling the player that the item isn't avaiable.
+     *
+     * @param itemName which is a item name String
+     */
+    public boolean pickUp(String itemName) {
+        Item item = ((SalesRoom) currentRoom).removeItem(itemName);
         if (item == null) {
-            System.out.print("The " + itemName + " you were looking for doesn't exist");
-        } else {
-            items.add(item);
+            //System.out.print("The " + itemName + " you were looking for doesn't exist");
+            return false;
         }
+        items.add(item);
+        return true;
     }
 
-    public void drop(String itemName) {
-        for (int i = 0; i < items.size(); i++){
-            if (items.get(i).getName().equals(itemName)){
+    /**
+     * The method removes an item from
+     *
+     * @param itemName
+     */
+    public boolean drop(String itemName) {
+        for (int i = 0; i < items.size(); i++) {
+            if (items.get(i).getName().equals(itemName)) {
                 Item item = items.remove(i);
-                currentRoom.addItem(item);
-                return;
+                ((SalesRoom) currentRoom).addItem(item);
+                return true;
             }
         }
-        // TODO throw message on item not found
+        return false;
     }
 
     public Room goRoom(String direction) {
         Room nextRoom = currentRoom.getExit(direction);
-        if (nextRoom != null){
+        if (nextRoom != null) {
             currentRoom = nextRoom;
         }
         return nextRoom;
     }
 
-    public int getLife() { 
+    public int getLife() {
         return life;
     }
 
@@ -72,8 +109,8 @@ public class Player {
     public void setItems(List<Item> items) {
         this.items = items;
     }
-    
-    public Room getCurrentRoom () {
+
+    public Room getCurrentRoom() {
         return currentRoom;
     }
 
@@ -84,14 +121,13 @@ public class Player {
     public void removeLife(int life) {
         this.life -= life;
     }
-    
+
     public void addMoney(int money) {
         this.money += money;
     }
-    
+
     public void removeMoney(int money) {
         this.money -= money;
     }
-    
-}
 
+}
