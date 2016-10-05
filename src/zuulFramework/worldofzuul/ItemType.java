f2c6c50e02231eadbe0cef42f9b30913bcf26d5f
Hugzy <frede.madsen3@gmail.com>
2016-10-05 15:45:17 +0200

    Added NameGenerator and random weight

--- src/zuulFramework/worldofzuul/ItemType.java
+++ src/zuulFramework/worldofzuul/ItemType.java
@@ -26,6 +26,7 @@ public enum ItemType {
      * @return vægten af et item af typen double.
      */
     public double getWeight() {
+        this.weight=this.weight*(Math.random()*0.5);
         return this.weight;
     }
 }
---
+++ src/zuulFramework/worldofzuul/NameGenerator.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zuulFramework.worldofzuul;
+/**
+ *
+ * @author frede
+ */
+public class NameGenerator {
+    static private String[] words = new String[45];
+
+    static void randomNames()
+    {
+        words[0] = "ÖJA";
+        words[1] = "PROG";
+        words[2] = "KLUB";
+        words[3] = "KE";
+        words[4] = "VAM";
+        words[5] = "BODA";
+        words[6] = "LITT";
+        words[7] = "HOPEN";
+        words[8] = "LÖR";
+        words[9] = "HES";
+        words[10] = "VEST";
+        words[11] = "LUTT";
+        words[12] = "TRA";
+        words[13] = "ÅTÅR";
+        words[14] = "KEN";
+        words[15] = "TER";
+        words[16] = "NITT";
+        words[17] = "FÖR";
+        words[18] = "INGA";
+        words[19] = "BÄR";
+        words[20] = "ALLÅ";
+        words[21] = "LACK";
+        words[22] = "KAV";
+        words[23] = "FTIG";
+        words[24] = "HOLM";
+        words[25] = "INGBY";
+        words[26] = "SUND";
+        words[27] = "GRUN";
+        words[28] = "JOD";
+        words[29] = "KUGGA";
+        words[30] = "LOPP";
+        words[31] = "LACK";
+        words[32] = "STÅL";
+        words[33] = "ÖMME";
+        words[34] = "NYGÅ";
+        words[35] = "APPA";
+        words[36] = "SVIK";
+        words[37] = "LEK";
+        words[38] = "JÄR";
+        words[39] = "GVES";
+        words[40] = "HJÄR";
+        words[41] = "ITTE";
+        words[42] = "OCKO";
+        words[43] = "ITYD";
+        words[44] = "ALBO";
+    }
+    
+    public static String pickRandomName(){
+       randomNames();
+       String name;
+       int pickRandomNumber=(int)(Math.random()*44);
+       int pickSecondRandomNumber=(int)(Math.random()*44);
+       name = words[pickRandomNumber] + words[pickSecondRandomNumber];
+       return name;
+    }
+
+}
