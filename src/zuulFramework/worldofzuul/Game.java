e5294d454adbb247a177274a7ae715d03535e303
TanyaXfun <tanja331@live.dk>
2016-10-05 12:49:22 +0200

    Made a "buy food" in canteen class

--- src/zuulFramework/worldofzuul/Game.java
+++ src/zuulFramework/worldofzuul/Game.java
@@ -32,12 +32,12 @@ public class Game {
 
         //Create all the room in the game (as an object)
         entrance = new Room("The entrance to Ikea");
-        exit = new Room("The exit of Ikea, you can pay for your stuff");
-        ballroom = new Room("Ballroom");
+        exit = new Exit("The exit of Ikea, you can pay for your stuff");
+        ballroom = new Ballroom("Ballroom");
         kitchen = new Room("Kitchen");
         dinningRoom = new Room("dinningRoom");
         livingRoom = new Room("livingRoom");
-        canteen = new Room("canteen");
+        canteen = new Canteen("canteen");
         bedroom = new Room("bedroom");
         childrensRoom = new Room("childrensRoom");
         electronics = new Room("electronics");
