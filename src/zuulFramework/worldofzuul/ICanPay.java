806f8eaed86dabc9af8bae9421fa0317b86aa702
Rasmus Hansen <rhdh@digizuite.com>
2016-10-12 11:58:17 +0200

    Update javadocs for ICanPay interface

--- src/zuulFramework/worldofzuul/ICanPay.java
+++ src/zuulFramework/worldofzuul/ICanPay.java
@@ -1,8 +1,17 @@
 package zuulFramework.worldofzuul;
 
 /**
- * Created by Rasmus Hansen .
+ * Implementing this interface means that it is possible to somehow pay in the implementer.
+ * This is mainly thought to be used in Room subclasses.
  */
 public interface ICanPay {
+    /**
+     * Pays something in the room
+     *
+     * @param player  The player who buys something
+     * @param command The buy command to execute
+     * @param game    The game instance running
+     * @return A boolean indicating if the purchase was successful
+     */
     boolean buy(Player player, Command command, Game game);
 }
