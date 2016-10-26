2714ba2e8842ad441804e33384574adad6ec4637
TanyaXfun <tanja331@live.dk>
2016-10-26 14:51:56 +0200

    #14
    fix pay in canteen class

--- src/zuulFramework/worldofzuul/Canteen.java
+++ src/zuulFramework/worldofzuul/Canteen.java
@@ -5,7 +5,6 @@ package zuulFramework.worldofzuul;
  */
 public class Canteen extends Room implements ICanPay {
 
-
     /**
      * @param description
      */
@@ -15,28 +14,40 @@ public class Canteen extends Room implements ICanPay {
 
     /**
      * Buys food, depends on how much life the player wants to heal himself with
+     *
      * @param player the player that wants to buy food
      * @param command the command that the player has inserted
      * @return always returns true when a player buys food
      */
     @Override
     public boolean buy(Player player, Command command, Game game) {
-        
-        // Use command to define how much life a player wishes for, depends on 
-        // how much food they buy
-        String secondWord = command.getSecondWord();
-        // Convert secondWord from string to int
-        int toBuyFor = Integer.parseInt(secondWord);
-        // The player gets life depends on how much food he bought 
-        player.addLife(toBuyFor);
-        // Use setter and getter method to calculate how much money a player has 
-        // used
-        player.removeMoney(toBuyFor);
-        
-        System.out.println("You bought food for " + toBuyFor);
-        
-        // return true because a player has bought food
-        return true;
-        
+
+        if (command.hasSecondWord()) {
+            // Use command to define how much life a player wishes for, depends on 
+            // how much food they buy
+
+            String secondWord = command.getSecondWord();
+            try {
+                // Convert secondWord from string to int
+                int toBuyFor = Integer.parseInt(secondWord);
+                // The player gets life depends on how much food he bought 
+                player.addLife(toBuyFor);
+                // Use setter and getter method to calculate how much money a player has 
+                // used
+                player.removeMoney(toBuyFor);
+
+                System.out.println("You bought food for " + toBuyFor + " $ ");
+
+                // return true because a player has bought food
+                return true;
+            } catch(NumberFormatException e) {
+                System.out.println("You need to enter a number");
+            }
+
+        } else {
+            System.out.println("You must specify how much you would by for");
+        }
+        return false;
     }
+
 }
