686f080eaeb1a87dfe5ca7c0feb4c38f62575f46
SanderumChristian <chund.Christianjohansen@gmail.com>
2016-11-16 15:19:56 +0100

    #33 Added weightBar and healthBar

--- src/zuulFramework/worldofzuul/gui/Controller.java
+++ src/zuulFramework/worldofzuul/gui/Controller.java
@@ -1,4 +1,30 @@
 package zuulFramework.worldofzuul.gui;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ProgressBar;
+import zuulFramework.worldofzuul.entities.Player;
+public class Controller implements Initializable  {
+    Player player = new Player();
+
+    @FXML
+    private ProgressBar healthBar;
+    @FXML
+    private ProgressBar weightBar;
+    
+    public void showHealthBar() {
+        this.healthBar.setProgress(((double)(player.getLife()))/100);
+    }
+    
+    public void showWeightBar() {
+        this.weightBar.setProgress(player.getCarryWeight()/100);
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        showHealthBar();
+        showWeightBar();     
+    }
 }
