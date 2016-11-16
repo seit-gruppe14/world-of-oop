6ca904e42a34a916a090528916fc96a30325c8ff
Hugzy <frede.madsen3@gmail.com>
2016-11-16 12:27:00 +0100

    changes to controller

--- src/zuulFramework/worldofzuul/gui/Controller.java
+++ src/zuulFramework/worldofzuul/gui/Controller.java
@@ -1,4 +1,18 @@
 package zuulFramework.worldofzuul.gui;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+
 
 public class Controller {
+    @FXML
+    private void handleButtonMoveEvent(ActionEvent event){
+        
+    }
 }
