c183ef3be4718afcb01af0bef65887d0716f1d00
Alexander Roed Thorup <alexander@roedthorup.dk>
2016-11-16 13:00:09 +0100

    Implemented basic functions

--- src/zuulFramework/worldofzuul/gui/Controller.java
+++ src/zuulFramework/worldofzuul/gui/Controller.java
@@ -1,4 +1,33 @@
 package zuulFramework.worldofzuul.gui;
 
+import java.awt.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
 public class Controller {
+
+    @FXML
+    private Button actionButtonPay;
+    @FXML
+    private Button actionButtonAsk;
+    @FXML
+    private Button actionButtonHelp;
+    @FXML
+    private TextArea textArea;
+    
+    @FXML
+    private void handleActionButtons(javafx.event.ActionEvent event) {
+        if(event.getSource() == actionButtonPay) {
+            textArea.appendText("Pay");
+        }
+        if(event.getSource() == actionButtonAsk) {
+            textArea.appendText("Ask");
+        }
+        
+        if(event.getSource() == actionButtonHelp) {
+            textArea.appendText("Help");
+        }
+       
+    }
 }
