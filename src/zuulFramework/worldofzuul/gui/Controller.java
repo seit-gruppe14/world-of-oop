3386816fac53d844cd40a2040db0c9a237d425e2
Hugzy <frede.madsen3@gmail.com>
2016-11-16 13:00:14 +0100

    Can write to console

--- src/zuulFramework/worldofzuul/gui/Controller.java
+++ src/zuulFramework/worldofzuul/gui/Controller.java
@@ -10,9 +10,34 @@ import javafx.scene.control.TextArea;
 import javafx.scene.image.ImageView;
 
 
+
 public class Controller {
+
+    @FXML
+    private Button North;
+    @FXML
+    private Button West;
+    @FXML
+    private Button South;
+    @FXML
+    private Button East;
+    @FXML
+    private TextArea textArea;
+    
     @FXML
     private void handleButtonMoveEvent(ActionEvent event){
-        
+        if (event.getSource()==North) {
+            System.out.println("You went north");
+            textArea.appendText("You went North \n");
+        } else if(event.getSource()==West) {
+            System.out.println("You went West");
+            textArea.appendText("You went West \n");
+        } else if(event.getSource()==South) {
+            System.out.println("You went south");
+            textArea.appendText("You went South \n");
+        }else{
+            System.out.println("You went east");
+            textArea.appendText("You went East \n");
+        }
     }
 }
