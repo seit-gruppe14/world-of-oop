86be881c348548f9c3c3e9a00c0925e76adbe692
Rasmus Hansen <rhdh@digizuite.com>
2016-11-18 16:02:01 +0100

    #36 add passive animation

--- src/zuulFramework/worldofzuul/entities/MovingEntity.java
+++ src/zuulFramework/worldofzuul/entities/MovingEntity.java
@@ -1,9 +1,16 @@
 package zuulFramework.worldofzuul.entities;
 
+import javafx.animation.Interpolator;
+import javafx.animation.PathTransition;
+import javafx.animation.Timeline;
 import javafx.collections.ObservableList;
 import javafx.scene.Node;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.LineTo;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
+import javafx.util.Duration;
 import zuulFramework.worldofzuul.gui.IDrawable;
 import zuulFramework.worldofzuul.gui.Offset;
 import zuulFramework.worldofzuul.gui.animations.MoveTransition;
@@ -16,6 +23,7 @@ import java.util.Set;
  * Describes an entity that can move around between rooms
  */
 public abstract class MovingEntity extends Entity implements IDrawable {
+    PathTransition waitingAnimation;
     private Circle drawed;
 
     public void move() {
@@ -42,19 +50,50 @@ public abstract class MovingEntity extends Entity implements IDrawable {
         updateDraw();
     }
 
-    public void addToScene(ObservableList<Node> drawAt, Offset offset, Paint color) {
+    void addToScene(ObservableList<Node> drawAt, Offset offset, Paint color) {
         offset = offset.add(Offset.getRandomOffsetForRoom());
         Circle circle = new Circle(offset.X, offset.Y, 5, color);
         drawAt.add(circle);
 
         this.drawed = circle;
+        addWaitingAnimation();
+    }
+
+    private void addWaitingAnimation() {
+        Path path = new Path();
+        path.getElements().add(new MoveTo(drawed.getCenterX(), drawed.getCenterY()));
+
+        Offset o = getCurrentRoom().getLocation().add(Offset.getRandomOffsetForRoom());
+        path.getElements().add(new LineTo(o.X, o.Y));
+
+        o = getCurrentRoom().getLocation().add(Offset.getRandomOffsetForRoom());
+        path.getElements().add(new LineTo(o.X, o.Y));
+
+        o = getCurrentRoom().getLocation().add(Offset.getRandomOffsetForRoom());
+        path.getElements().add(new LineTo(o.X, o.Y));
+
+        path.getElements().add(new LineTo(drawed.getCenterX(), drawed.getCenterY()));
+
+        PathTransition pathTransition = new PathTransition();
+        pathTransition.setDuration(Duration.millis(10000 + ((int) Math.random() * 3000)));
+        pathTransition.setPath(path);
+        pathTransition.setNode(this.drawed);
+        pathTransition.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
+        pathTransition.setCycleCount(Timeline.INDEFINITE);
+        pathTransition.setAutoReverse(false);
+        pathTransition.setInterpolator(Interpolator.LINEAR);
+        pathTransition.play();
+
+        waitingAnimation = pathTransition;
     }
 
     @Override
     public void updateDraw() {
         if (drawed != null) {
+            waitingAnimation.stop();
             Offset o = getCurrentRoom().getLocation().add(Offset.getRandomOffsetForRoom());
             MoveTransition mt = new MoveTransition(drawed, o.X, o.Y);
+            mt.setOnFinished(event -> addWaitingAnimation());
             mt.play();
         }
     }
