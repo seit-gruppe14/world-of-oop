2e684ed3685f2e4f38b2936be7acc07e32482f35
Hugzy <frede.madsen3@gmail.com>
2016-12-16 13:45:14 +0100

    game gui param tag

--- src/zuulFramework/worldofzuul/gui/GameGUI.java
+++ src/zuulFramework/worldofzuul/gui/GameGUI.java
@@ -20,7 +20,11 @@ public class GameGUI extends Application {
      * The start method is called after the init method has returned,
      * and after the system is ready for the application to begin running.
      * @param primaryStage
-     * @throws Exception 
+     * primary stage for this application, onto which
+     * the application scene can be set. The primary stage will be embedded in
+     * the browser if the application was launched as an applet.
+     * Applications may create other stages, if needed, but they will not be
+     * primary stages and will not be embedded in the browser.
      */
     @Override
     public void start(Stage primaryStage) throws Exception {
