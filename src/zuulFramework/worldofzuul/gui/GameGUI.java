5970a9d9fe2a6b071dd0a5a5aaf0097e6d24093c
Hugzy <frede.madsen3@gmail.com>
2016-12-16 13:21:31 +0100

    Javadoc for guiv2

--- src/zuulFramework/worldofzuul/gui/GameGUI.java
+++ src/zuulFramework/worldofzuul/gui/GameGUI.java
@@ -7,11 +7,21 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class GameGUI extends Application {
-
+    /**
+     * Starts the program
+     * @param args 
+     */
     public static void startGUI(String[] args) {
         launch(args);
     }
-
+    
+    /**
+     * The main entry point for all JavaFX applications.
+     * The start method is called after the init method has returned,
+     * and after the system is ready for the application to begin running.
+     * @param primaryStage
+     * @throws Exception 
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
